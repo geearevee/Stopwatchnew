89b48a655ca00d26e7414281c59e28851f603f62
Gaurav <grv33431@gmail.com>
2023-09-01 07:57:35 +0530

    promises used

--- reactpractice/src/App.jsx
+++ reactpractice/src/App.jsx
@@ -3,10 +3,11 @@ import Counter from "./Counter";
 import Todo from "./Todo";
 import Api from "./Api";
 import Stopwatch from "./Stopwatch";
+import Promises from "./Promises";
 function App() {
   return (
     <>
-      <Stopwatch />
+      <Promises />
     </>
   );
 }
